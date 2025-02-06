ea77645ed9b9c83232e8863958b14ab4d39455ae
vikasr <vikasr@winjit.com>
2025-02-06 10:37:02 +0530

    Added minor changes

--- setup.py
+++ setup.py
@@ -9,21 +9,24 @@ def get_requirements(file_path:str)-> List[str]: # type: ignore
 
     """
     #need not come -e in setup file for installation in this get_requirements method
-    HYPEN_E_DOT = '-e .'
+    HYPEN_E_DOT = '-e .' 
     requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readline()
+        requirements=file_obj.readlines()#readline()
+        print(">>>>>>>>>>>.",requirements)
         requirements = [req.replace("\n","") for req in requirements]
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    
-    setup(
+    return requirements
 
-    name="mlproject",
-    version="0.0.1",
-    author="vikasrathodml@gmail.com",
-    packages=find_packages(),
-    install_requires = get_requirements("requirements.txt")
-    #install_requires = ["pandas","numpy","seaborn"]
+    
+setup(
+
+name="mlproject",
+version="0.0.1",
+author="vikasrathodml@gmail.com",
+packages=find_packages(),
+install_requires = get_requirements("requirements.txt")
+#install_requires = ["pandas","numpy","seaborn"]
 )
